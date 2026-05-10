c2ded59ee5337007bbf3690bac0dc612df327473
Alberto.vleite <albertotj76jw@gmail.com>
2026-05-10 19:03:19 -0300

    Projeto CrisPetHospedagem

--- js/script.js
+++ js/script.js
@@ -1,6 +1,34 @@
-console.log("Site carregado com sucesso!"); 
- 
-function mensagem(){ 
-   alert("Mensagem enviada com sucesso!"); 
-} 
- 
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll", () => {
+
+if(window.scrollY > 300){
+topBtn.style.display = "block";
+}else{
+topBtn.style.display = "none";
+}
+
+});
+
+topBtn.addEventListener("click", () => {
+
+window.scrollTo({
+top:0,
+behavior:"smooth"
+});
+
+});
+
+// animação cards
+
+const cards = document.querySelectorAll('.service-box');
+
+cards.forEach(card => {
+
+card.addEventListener('mouseenter', () => {
+
+card.style.transform = 'translateY(-10px)';
+
+});
+
+});
